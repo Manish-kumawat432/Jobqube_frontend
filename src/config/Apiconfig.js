661ee800f5e6c2e83c43ae1ef583fab4afd91f35
Manish Kumawat <ratiwal1582006@gmail.com>
2026-01-27 09:57:44 +0530

    ready for vercleq

--- src/config/Apiconfig.js
+++ src/config/Apiconfig.js
@@ -1,3 +1,3 @@
 export const url = "";
 export const REACT_APP_API_BASE = 'https://jobeqube-backend-id54.onrender.com/api/v1';
-// export const PORT='https://jobeqube-backend-id54.onrender.com/api/v1/'
+export const PORT='https://jobeqube-backend-id54.onrender.com/api/v1/'
